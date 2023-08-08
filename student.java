7d12a353cdece394519eb64e1f857dabbe1e7cb7
Aadhi <88002673+Aadhi41@users.noreply.github.com>
2023-08-08 14:53:41 +0530

    Add files via upload

---
+++ student.java
@@ -0,0 +1,32 @@
+// Encapsulation: Creating classes with private attributes and methods
+
+class Student {
+    private String name;
+    private int age;
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public static void main(String[] args) {
+        Student student = new Student("Alice", 20);
+        System.out.println("Student: " + student.getName() + ", Age: " + student.getAge());
+    }
+}
